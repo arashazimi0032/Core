ec87cdb597e800ef356582c0597b8c6229d25d68
arashazimi0032 <arashazimi0032@gmail.com>
2024-02-25 19:40:57 +0330

    base command repository added

---
+++ Core.Domain/IRepositories/IBaseCommandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain.BaseModels;
+
+namespace Core.Domain.IRepositories;
+
+public interface IBaseCommandRepository<TEntity, TId>
+    where TEntity : Entity<TId>
+    where TId : notnull
+{
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task AddRangeAsync(params TEntity[] entities);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    TEntity Update(TEntity entity);
+    void UpdateRange(params TEntity[] entity);
+    void UpdateRange(IEnumerable<TEntity> entity);
+    TEntity Remove(TEntity entity);
+    void RemoveRange(params TEntity[] entity);
+    void RemoveRange(IEnumerable<TEntity> entity);
+}
--- Core.Infrastructure/Repositories/Commands/BaseCommandRepository.cs
+++ Core.Infrastructure/Repositories/Commands/BaseCommandRepository.cs
@@ -1,5 +1,65 @@
-﻿namespace Core.Infrastructure.Repositories.Commands;
+﻿using Core.Domain.BaseModels;
+using Core.Domain.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
-public abstract class BaseCommandRepository
+namespace Core.Infrastructure.Repositories.Commands;
+
+public abstract class BaseCommandRepository<TContext, TEntity, TId> : IBaseCommandRepository<TEntity, TId>
+    where TContext : DbContext
+    where TEntity : Entity<TId>
+    where TId : notnull
 {
+    private readonly TContext _context;
+    protected DbSet<TEntity> dbSet;
+
+    protected BaseCommandRepository(TContext context)
+    {
+        _context = context;
+        dbSet = _context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        return (await dbSet.AddAsync(entity, cancellationToken)).Entity;
+    }
+
+    public async Task AddRangeAsync(params TEntity[] entities)
+    {
+        await dbSet.AddRangeAsync(entities);
+    }
+
+    public async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        await dbSet.AddRangeAsync(entities, cancellationToken);
+    }
+
+    public TEntity Remove(TEntity entity)
+    {
+        return dbSet.Remove(entity).Entity;
+    }
+
+    public void RemoveRange(params TEntity[] entity)
+    {
+        dbSet.RemoveRange(entity);
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entity)
+    {
+        dbSet.RemoveRange(entity);
+    }
+
+    public TEntity Update(TEntity entity)
+    {
+        return dbSet.Update(entity).Entity;
+    }
+
+    public void UpdateRange(params TEntity[] entity)
+    {
+        dbSet.UpdateRange(entity);
+    }
+
+    public void UpdateRange(IEnumerable<TEntity> entity)
+    {
+        dbSet.UpdateRange(entity);
+    }
 }
