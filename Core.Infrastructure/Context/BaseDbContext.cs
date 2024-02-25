6ab77b673ab24eb650f5427f158a2b802f1d12c6
arashazimi0032 <arashazimi0032@gmail.com>
2024-02-25 16:01:44 +0330

    add base contexts

---
+++ Core.Infrastructure/Context/BaseDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure.Context;
+
+public abstract class BaseDbContext<TContext> : DbContext
+    where TContext : DbContext
+{
+    protected BaseDbContext(DbContextOptions<TContext> options) : base(options)
+    {
+    }
+}
---
+++ Core.Infrastructure/Context/BaseIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure.Context;
+
+public abstract class BaseIdentityDbContext<TContext, TUser> : IdentityDbContext<TUser>
+    where TContext : IdentityDbContext<TUser>
+    where TUser : IdentityUser
+{
+    protected BaseIdentityDbContext(DbContextOptions<TContext> options) : base(options)
+    {
+    }
+}
