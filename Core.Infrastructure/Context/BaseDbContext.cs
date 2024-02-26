0ba5bfea1a3e2c3801f25c1e9687f3d2af4e8c30
arashazimi0032 <arashazimi0032@gmail.com>
2024-02-27 00:45:11 +0330

    auditing completed

--- Core.Infrastructure/Context/BaseDbContext.cs
+++ Core.Infrastructure/Context/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Domain.Primitives;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.Infrastructure.Context;
 
@@ -7,5 +8,25 @@ public abstract class BaseDbContext<TContext> : DbContext
 {
     protected BaseDbContext(DbContextOptions<TContext> options) : base(options)
     {
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<IAuditable>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(e => e.CreatedAt).CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(e => e.ModifiedAt).CurrentValue = DateTime.Now;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
--- Core.Infrastructure/Context/BaseIdentityDbContext.cs
+++ Core.Infrastructure/Context/BaseIdentityDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Core.Domain.Primitives;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,25 @@ public abstract class BaseIdentityDbContext<TContext, TUser> : IdentityDbContext
 {
     protected BaseIdentityDbContext(DbContextOptions<TContext> options) : base(options)
     {
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<IAuditable>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(e => e.CreatedAt).CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(e => e.ModifiedAt).CurrentValue = DateTime.Now;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
