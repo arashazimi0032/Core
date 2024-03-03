d25a157b950914a4cc1dfc46e11bc5eb4ac5b39d
arashazimi0032 <arashazimi0032@gmail.com>
2024-03-03 22:08:09 +0330

    CleanBaseException Added (Base For Global Exception Handling in future) Nuget Version: 3.0.3

---
+++ Core/Domain/Exceptions/CleanBaseException.cs
@@ -0,0 +1,35 @@
+﻿using Core.Application.ServiceLifeTimes;
+using System.Net;
+
+namespace Core.Domain.Exceptions;
+
+public abstract class CleanBaseException<TCode> : SystemException, ICleanBaseIgnore
+    where TCode : Enum
+{
+    public abstract TCode ExceptionCode { get; }
+
+    public virtual HttpStatusCode HttpStatusCode => HttpStatusCode.InternalServerError;
+
+    public virtual string ResourceKey => Convert.ToInt32(ExceptionCode).ToString();
+
+    protected CleanBaseException()
+    {
+    }
+    protected CleanBaseException(string? message) 
+        : base(message)
+    {
+    }
+    protected CleanBaseException(string? message, Exception? innerException) 
+        : base(message, innerException)
+    {
+    }
+
+    public override string ToString()
+    {
+        string fullMessage = InnerException is null ? 
+            $"Message: {Message}" : 
+            $"Message: {Message} \n InnerExceptionMessage: {InnerException.Message}";
+
+        return $"Clean Template Exception: \n {fullMessage}";
+    }
+}
