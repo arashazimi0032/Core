c852898fe288eba9b4a1b84510f80f6aff791945
arashazimi0032 <arashazimi0032@gmail.com>
2024-03-23 00:41:52 +0330

    some performance optimization in structural equality of CleanBaseEntity

--- Core/Domain/BaseModels/CleanBaseEntity.cs
+++ Core/Domain/BaseModels/CleanBaseEntity.cs
@@ -6,11 +6,11 @@ public abstract class CleanBaseEntity<TId> : CleanEntity, IEquatable<CleanBaseEn
     public TId Id { get; set; }
     public bool Equals(CleanBaseEntity<TId>? other)
     {
-        return Equals((object?)other);
+        return other is not null && Id.Equals(other.Id);
     }
     public override bool Equals(object? obj)
     {
-        return obj is CleanBaseEntity<TId> entity && Id.Equals(entity.Id);
+        return obj is not null && obj is CleanBaseEntity<TId> entity && Id.Equals(entity.Id);
     }
     public static bool operator ==(CleanBaseEntity<TId> left, CleanBaseEntity<TId> right)
     {
